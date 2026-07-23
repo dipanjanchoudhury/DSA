40b45a8880d9928385b51411c7a6be8ae70e3b77
Dipanjan Choudhury <dipanjanchaudhary6@gmail.com>
2026-07-23 18:21:20 +0530

    Time: 13 ms (26.12%) | Memory: 47.2 MB (60.56%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> ans = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0)
+            return ans;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words)
+            map.put(w, map.getOrDefault(w, 0) + 1);
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> seen = new HashMap<>();
+
+            for (int j = i; j + wordLen <= s.length(); j += wordLen) {
+                String word = s.substring(j, j + wordLen);
+
+                if (map.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (seen.get(word) > map.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+                        ans.add(left);
+
+                        String leftWord = s.substring(left, left + wordLen);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+                } else {
+                    seen.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
